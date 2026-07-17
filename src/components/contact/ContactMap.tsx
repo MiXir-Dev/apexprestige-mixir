41a9d3ee7a39f56dad45c1b59e7fdeeb9131d96f
simy46 <missipsa.annane@polymtl.ca>
2026-07-17 18:00:33 -0400

    Update Google Maps iframe source and title for accuracy

--- src/components/contact/ContactMap.tsx
+++ src/components/contact/ContactMap.tsx
@@ -3,14 +3,14 @@ const ContactMap = () => {
     <div className="lg:col-span-2">
       <div className="h-96 rounded-lg overflow-hidden shadow-md">
         <iframe
-          src="https://www.google.com/maps/d/u/0/embed?mid=1keB2LM1SVq9fHTwvC12os0xJ5si6Epo&ehbc=2E312F&noprof=1"
+          src="https://www.google.com/maps/d/u/0/embed?mid=1XW_y7kItYwQmapsyKUXhjsFURdpS5l8&ehbc=2E312F&noprof=1"
           width="100%"
           height="100%"
           style={{ border: 0 }}
           allowFullScreen={true}
           loading="lazy"
           referrerPolicy="no-referrer-when-downgrade"
-          title="Carte Google Maps montrant les zones desservies par Mr. Clear"
+          title="Carte Google Maps montrant les zones desservies par Apex Prestige"
         ></iframe>
       </div>
     </div>
